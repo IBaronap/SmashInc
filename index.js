f07a3a71839e92ee0b9d074bdb04a423d023c1e1
Karolfc <karolfuentescaro@gmail.com>
2023-02-23 23:29:54 -0500

    Interfaces

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ import { Server } from 'socket.io';
 const expressApp = express(); //Environment setup
 const PORT = 5050;
 
+//Mupi
 expressApp.use(express.json()) //Middlewares
 expressApp.use('/mupi-home', express.static('public-home')); //Middlewares
 expressApp.use('/mupi-connected', express.static('public-connected')); 
 expressApp.use('/mupi-gameover', express.static('public-gameover')); 
 expressApp.use('/mupi-disconnect', express.static('public-disconnect')); 
+//Phone
+expressApp.use('/phone-connected', express.static('public-phone-connected'));
+expressApp.use('/phone-controller', express.static('public-phone-controller'));
+expressApp.use('/phone-gameover', express.static('public-phone-gameover'));
+expressApp.use('/phone-form', express.static('public-phone-form'));
+expressApp.use('/phone-disconnect', express.static('public-phone-disconnect'));
 
 // expressApp.listen(PORT);
 const httpServer = expressApp.listen(PORT, () => { //Star the server
---
+++ public-phone-form/sketch.js
@@ -0,0 +1,22 @@
+let socket = io("http://localhost:5050", { path: '/real-time' })
+let canvas;
+let controllerX, controllerY = 0;
+
+function setup() {
+    canvas = createCanvas(windowWidth, windowHeight);
+    canvas.style('z-index', '-1');
+    canvas.style('position', 'fixed');
+    canvas.style('top', '0');
+    canvas.style('right', '0');
+    background(0);
+}
+
+function draw() {
+}
+
+function mouseDragged() {
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
